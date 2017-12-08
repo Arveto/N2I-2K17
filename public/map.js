4ecfac8f263d875d57d73f3d7923b682a9546c01
TERRUSS <31973542+TERRUSS@users.noreply.github.com>
2017-12-08 01:30:46 +0100

    Add files via upload
    Center : france

--- public/map.js
+++ public/map.js
@@ -4,21 +4,30 @@ var map;
 (function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 5,
-    center: {lat: -25.363, lng: 131.044}
+    zoom: 6,
+    center: {lat: 47.276358, lng: 2.436975}
   });
 
 
   // Display one event onthe map, add event
-  window.dispPoints = function (points) {      //display tab of points
-    for (let i = 0; i < points.length; i++) {
-      let coords = points[i].coord;
+  window.dispPoints = function (point) {      //display tab of points
+    for (let i = 0; i < point.length; i++) {
+      let coords = point[i].coord;
       console.log(coords);
 
       let marker = new google.maps.Marker({
         position: coords,
         map: map
       });
+
+      marker.addListener('click', function() {
+        map.setZoom(15);
+        map.setCenter(marker.getPosition());
+
+        // socket.emit("joinEvent", point[i].eventId);
+        console.log("emit eventId " + point[i].eventId);
+      });
+
     }
   }
 })();
@@ -26,21 +35,14 @@ var map;
 
 
 var points = [ {
-                coord: {lat: -33.875, lng: 151.056}
+                coord: {lat: -33.875, lng: 151.056},
+                eventId: 42
               },
               {
-                coord: {lat: -33.673, lng: 150.447}
+                coord: {lat: -33.673, lng: 150.447},
+                eventId: 666
               } ];
 
 dispPoints(points);
 
-  // var marker = ({
-  //   position: {lat: -25.363, lng: 131.044},
-  //   map: map,
-  //   title: 'Ouvrir la discussion'
-  // });
-  //
-  // marker.addListener('click', () => {
-  //   console.log("OUVERTURE DU CHAT " + this.evenement);
-  // });
 },false);
