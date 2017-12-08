12f563f7a61ac1ac11ca351c5dfe1f7d21306703
TERRUSS <31973542+TERRUSS@users.noreply.github.com>
2017-12-08 02:04:50 +0100

    Add files via upload

---
+++ SAMgames/courtepaille.js
@@ -0,0 +1,31 @@
+
+var app = new PIXI.Application(600, 600, {backgroundColor : 0xffffff});
+document.body.appendChild(app.view);
+
+  //canva content
+var arrow = PIXI.Sprite.fromImage('./arrow.png');
+
+var toggle = false;
+var foo = Math.floor((Math.random() * 10) + 1);
+
+arrow.anchor.set(0.5);   //center the anchor
+
+arrow.x = app.renderer.width/2; //center pic in the canva
+arrow.y = app.renderer.height/2;
+
+app.stage.addChild(arrow); //insert pic in canva
+
+arrow.interactive = true;
+arrow.buttonMode = true;
+
+arrow.on('click', () => {
+    app.ticker.add( (delta) => {
+      arrow.rotation += 0.06 * delta;
+    });
+    foo--;
+    console.log(foo);
+
+    if (!foo){
+      app.ticker.stop();
+    }
+})
